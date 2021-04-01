05587eb14da9b6b91346532475dfbcb9ec322ee8
sherkt1 <sherkt1@udayton.edu>
2021-03-31 20:43:07 -0400

    Create get_notes.py

---
+++ get_notes.py
@@ -0,0 +1,9 @@
+import pretty_midi
+midi_data = pretty_midi.PrettyMIDI('C:/Users/thoma/Downloads/BanjoKazooie - Pause Menu.mid')
+
+print("duration:",midi_data.get_end_time())
+print(f'{"note":>10} {"start":>10} {"end":>10}')
+for instrument in midi_data.instruments:
+    print("instrument:", instrument.program);
+    for note in instrument.notes:
+        print(f'{note.pitch:10} {note.start:10} {note.end:10}')
