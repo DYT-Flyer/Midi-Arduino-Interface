e826b22e506027ec284743f2fb2d8920247a3c48
sherkt1 <sherkt1@udayton.edu>
2021-04-02 10:41:53 -0400

    Added Genre
    Added Genre

--- get_notes.py
+++ get_notes.py
@@ -1,4 +1,10 @@
 import pretty_midi
+
+#For Plotting
+import mir_eval.display
+import librosa.display
+import matplotlib.pyplot as plt
+
 midi_data = pretty_midi.PrettyMIDI('C:/Users/thoma/Downloads/BanjoKazooie - Pause Menu.mid')
 
 print("duration:",midi_data.get_end_time())
@@ -7,5 +13,3 @@ for instrument in midi_data.instruments:
     print("instrument:", instrument.program);
     for note in instrument.notes:
         print(f'{note.pitch:10} {note.start:10} {note.end:10}')
-		print('hey')
-		j
