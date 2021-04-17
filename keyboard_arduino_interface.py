a6b060928b4141428aa99f5cbf744f436b150881
sherkt1 <54909622+sherkt1@users.noreply.github.com>
2021-04-16 22:15:18 -0400

    Moved Blues Dataset out

---
+++ keyboard_arduino_interface.py
@@ -0,0 +1,89 @@
+import pygame
+from pygame.locals import *
+from pygame import midi
+from datetime import datetime
+#from apscheduler.scheduler import Scheduler
+
+#3,4,5
+
+def printMIDIDeviceList():
+    for i in range(pygame.midi.get_count()):
+        print(pygame.midi.get_device_info(i), i)
+            
+import pygame
+from pygame import midi as pygame_midi
+from pygame.midi import midis2events
+
+pygame.init()
+pygame.font.init()
+pygame_midi.init()
+
+print("Found {} midi devices".format(pygame_midi.get_count()))
+
+for i in range(pygame_midi.get_count()):
+    print("#{}: {}".format(i, pygame_midi.get_device_info(i)))
+
+
+midi_input_id = pygame_midi.get_default_input_id()
+midi_input = pygame_midi.Input(midi_input_id)
+
+print("Using input #{}".format(midi_input_id))
+
+
+
+tstart = datetime.now()
+tstart = tstart.hour*3600+tstart.minute*60+tstart.second
+
+#sched.add_interval_job(update_piano_roll(), seconds = 5)
+
+def update_piano_roll(piano_roll,new_notes):
+    piano_roll[:,0:19] = piano_roll[:,20:24]
+    piano_roll[:,20:24] = new_notes
+
+def extract_events(events):
+    #List is sequential
+    #Thus: if first item in list has status: 128, skip
+    #If Last item in list has status 144, skip
+    current_event = events.pop(0)
+    current_note = current_event.data1
+    current_events = []
+    
+    for i in range(0,len(events)):
+        if events[i].data1 == current_note:
+            
+        
+    
+    
+    
+    
+    pause()
+    
+#Status: 144-Push Down
+#Status: 128-Let Up
+    
+while True:
+    if not midi_input.poll():
+        continue
+
+    events = midis2events(midi_input.read(40), midi_input_id)
+    for event in events:
+        print(event)
+
+    #scheduler.run()
+    
+
+
+plt.plot(x, y, color='green', linestyle='dashed', linewidth = 3,
+         marker='o', markerfacecolor='blue', markersize=12)
+
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+sched = Scheduler()
+sched.start()
+
+sched.shutdown()
+
