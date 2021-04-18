c30ab84488798b081579b30e1d714a3a85b20391
sherkt1 <54909622+sherkt1@users.noreply.github.com>
2021-04-18 13:05:17 -0400

    Update keyboard_arduino_interface.py

--- keyboard_arduino_interface.py
+++ keyboard_arduino_interface.py
@@ -3,86 +3,87 @@ from pygame.locals import *
 from pygame import midi
 from datetime import datetime
 import math
-#from apscheduler.scheduler import Scheduler
+import pygame
+from pygame import midi as pygame_midi
+from pygame.midi import midis2events
+
+from apscheduler.scheduler import Scheduler
 
 #3,4,5
 
 def printMIDIDeviceList():
     for i in range(pygame.midi.get_count()):
         print(pygame.midi.get_device_info(i), i)
-            
-import pygame
-from pygame import midi as pygame_midi
-from pygame.midi import midis2events
 
-pygame.init()
-pygame.font.init()
-pygame_midi.init()
-
-print("Found {} midi devices".format(pygame_midi.get_count()))
-
-for i in range(pygame_midi.get_count()):
-    print("#{}: {}".format(i, pygame_midi.get_device_info(i)))
-
-
-midi_input_id = pygame_midi.get_default_input_id()
-midi_input = pygame_midi.Input(midi_input_id)
-
-print("Using input #{}".format(midi_input_id))
-
-
+# print("Found {} midi devices".format(pygame_midi.get_count()))
+# for i in range(pygame_midi.get_count()):
+#     print("#{}: {}".format(i, pygame_midi.get_device_info(i)))
+# print("Using input #{}".format(midi_input_id))
 
 tstart = datetime.now()
-tstart = tstart.hour*3600+tstart.minute*60+tstart.second
 
-#sched.add_interval_job(update_piano_roll(), seconds = 5)
-
-def update_piano_roll(piano_roll,new_notes):
-    piano_roll[:,0:19] = piano_roll[:,20:24]
-    piano_roll[:,20:24] = new_notes
-
-def extract_events(events):
-    #List is sequential
-    #Thus: if first item in list has status: 128, skip
-    #If Last item in list has status 144, skip
+class midi_event:
     
-    eventpair = []
-    eventpairs = []
-    
-    #Pairing begining and end notes
-    control = 1
-    while len(events)>0:
-        #Skip if first note is remainder from previous
-        current_event = events.pop(0)
-        if current_event.status == 128 and control == 1:
-            continue
+    def __init__(self, steps_per_second, number_of_notes):
+        pygame.init()
+        pygame.font.init()
+        pygame_midi.init()
         
-        control = 0
-        current_note = current_event.data1
+        self.midi_input_id = pygame_midi.get_default_input_id()
+        self.midi_input = pygame_midi.Input(self.midi_input_id)
+        self.tbegin = datetime.now().hour*3600+datetime.now().minute*60+datetime.now().second
         
-        # matches note begin to note end
-        for i in range(0,len(events)):
-            if events[i].data1 == current_note:
-                eventpair.append(current_event)
-                eventpair.append(events.pop(i))
-                eventpairs.append(eventpair)
-                eventpair = []
-                break
-    
-    return eventpairs
+        sched.add_interval_job(self.update_piano_roll(), seconds = 5)
+        
+        self.number_of_notes = number_of_notes
+        self.steps_per_second = steps_per_second
+        self.eventpairs = []
+        
+    def update_piano_roll(self):
+        self.extract_events()
+        self.extract_time()
+        self.piano_roll[:,0:19] = self.piano_roll[:,20:24]
+        for note in self.upr:
+            self.piano_roll[note[0],note[1]:note[2]] = 1
 
-def extract_time(eventpairs, current_note, n, tb):
-    for i in range(0,len(eventpairs)):
-        te = eventpairs[i][1].time()
-        ts = eventpairs[i][0].time()
-        tint = math.floor((te - ts)*n)
-        update_piano_roll(current_note, ts-tb, tint-tb)
+    def extract_events(self):
+        #List is sequential
+        #Thus: if first item in list has status: 128, skip
+        #If Last item in list has status 144, skip
         
+        eventpair = []
+        eventpairs = []
         
-        
+        #Pairing begining and end notes
+        control = 1
+        while len(self.events)>0:
+            #Skip if first note is remainder from previous
+            current_event = self.events.pop(0)
+            if current_event.status == 128 and control == 1:
+                continue            
+            control = 0
+            current_note = current_event.data1
             
-            
+            # matches note begin to note end
+            for i in range(0,len(self.events)):
+                if self.events[i].data1 == current_note:
+                    eventpair.append(current_event)
+                    eventpair.append(self.events.pop(i))
+                    eventpairs.append(eventpair)
+                    eventpair = []
+                    break
     
+        self.eventpairs = eventpairs
+
+    def extract_time(self):
+        self.upr = []
+        for i in range(0,len(self.eventpairs)):
+            te = self.eventpairs[i][1].time()
+            ts = self.eventpairs[i][0].time()
+            current_note = self.eventpairs[i][0].data1
+            tint = math.floor((te - ts)*self.steps_per_second)
+            self.upr.append([current_note, ts-self.tbegin, tint-self.tbegin])
+        
 #Status: 144-Push Down
 #Status: 128-Let Up
     
